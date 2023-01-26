1358af639eb3294130bb3d2bdc7831b5dc362200
Stoyan Todorov <c.todopob@gmail.com>
2023-01-26 20:26:40 +0200

    updateControls

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//module requirements 
 const express = require('express')
 const hbs = require('hbs')
 const mongoose = require('mongoose')
@@ -5,49 +6,70 @@ const Item = require('./utils/item')
 const path = require('path')
 const bodyParser = require('body-parser')
 
+//paths
 const publicDirectoryPath = path.join(__dirname, './public')
 const viewsPath = path.join(__dirname,'./templates/views')
 const partialsPath = path.join(__dirname,'./templates/views/partials')
 
+//express & middleware setup
 const app = express()
-
 app.use(bodyParser.urlencoded({extended:false}))
 app.use('/public',express.static(publicDirectoryPath))
 app.set('views',viewsPath)
 app.set('view engine','hbs')
 
+//handlebars setup
 hbs.registerPartials(partialsPath)
 hbs.registerHelper('JSONStringify',(object)=>{
     return JSON.stringify(object)
 })
 
+//database connection
 mongoose.connect('mongodb://127.0.0.1/27017',{useNewUrlParser:true},{useUnifiedTopology:true})
 .then((result)=>{app.listen(3000)}).catch((err)=>{console.log(err)})
 
+//routing
 app.get('/controls',(req,res)=>{
     res.render('controls',{
         title:'Controls'
     })
 })
-app.get('/create',(req,res)=>{
+app.get('/create',(req,res)=>{ 
     res.render('controls',{
         title:'Controls - Create'
     })
 })
-app.get('/update',(req,res)=>{
-    console.log(req.query)
-    res.redirect('/all')
-})
-app.get('/delete',(req,res)=>{
-    console.log(req.query)
-    res.redirect('/all')
-})
-// app.get('/submit',(req,res)=>{
-//     console.log(req.query)
-//     res.send('item added')
-// })
 
-app.get('/submit',(req,res)=>{
+//controls & routing utils (NEEDS TO BE LESS VERBOSE! UPDATE IN THE NEXT COMMIT)
+app.get('/update',(req,res)=>{ //<- sends the Item for update
+    Item.findById({_id:req.query.id}).then((result)=>{
+        res.render('edit',{
+            title:result._id,    
+            name:result.name,
+            price:result.price,
+            qty:result.qty,
+            description:result.description
+        })
+    })     
+})
+app.get('/updated',(req,res)=>{ // <- updates the selected item in the collection
+    Item.updateOne({_id:req.query.id},{
+        name:req.query.name,
+        price:req.query.price,
+        qty:req.query.qty,
+        description:req.query.description
+    }).then((result)=>{
+        res.redirect('/all')
+    })
+})
+app.get('/delete',(req,res)=>{ // <- deletes an item from store collection
+    Item.deleteOne({_id:req.query.id}).then((result)=>{
+        res.redirect('/all')
+    })
+    
+})
+
+app.get('/submit',(req,res)=>{ // <- creates a new Item in Store collection
     const imgUrlPrePath = './public'
     const item = new Item({
         name:req.query.name,
@@ -64,15 +86,13 @@ app.get('/submit',(req,res)=>{
         console.log(err)
     })
 })
-    app.get('/all',(req,res)=>{
+    app.get('/all',(req,res)=>{ // <- list all Items currently in Store collection
         Item.find()
         .then((result)=>{
             res.render('index',{
                 title:'Items',
                 data:result
             })
-            //console.log(result)
         }).catch((err)=>{console.log(err)})
     })
 
-console.log(partialsPath)
